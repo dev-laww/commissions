e27ac1e4256677f9f604725ffc3032efba4734ac
tora-o <97511605+tora-o@users.noreply.github.com>
2023-03-10 18:56:34 +0800

    java: updated status effect

--- java/kenshin/pokemon/src/StatusEffect.java
+++ java/kenshin/pokemon/src/StatusEffect.java
@@ -18,4 +18,8 @@ public class StatusEffect {
     public void reduceDuration() {
         duration--;
     }
+
+    public static StatusEffect SLEEP = new StatusEffect("Sleep", 1);
+    public static StatusEffect ELECTROCUTED = new StatusEffect("Electrocuted", 3);
+    public static StatusEffect FEAR = new StatusEffect("Fear", 3);
 }
