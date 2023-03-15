c1f960e519695159ac697a7a9646fddcf4b9fe1b
tora-o <97511605+tora-o@users.noreply.github.com>
2023-03-15 22:20:34 +0800

    java: created baseclasses

---
+++ java/gominsi/LibraryManagementSystem/src/module2_excercise2_1_datatypesAndOperator/Author.java
@@ -0,0 +1,5 @@
+package module2_excercise2_1_datatypesAndOperator;
+
+public class Author {
+
+}
---
+++ java/gominsi/LibraryManagementSystem/src/module2_excercise2_1_datatypesAndOperator/BookAquisitionRunner.java
@@ -0,0 +1,4 @@
+package module2_excercise2_1_datatypesAndOperator;
+
+public class BookAquisitionRunner {
+}
