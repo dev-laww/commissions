898835d18fefd761481c3302a1b40e3553061717
tora-o <97511605+tora-o@users.noreply.github.com>
2023-03-18 19:36:45 +0800

    java: finished AccountDebit

--- java/shinji/AccountDebit/src/AccountRun.java
+++ java/shinji/AccountDebit/src/AccountRun.java
@@ -10,16 +10,19 @@ public class AccountRun {
         account1.setBalance(150.00);
         account2.setBalance(500.53);
 
-        System.out.printf("account1 balance: $%.2f account2 balance: $%.2f\n", account1.getBalance(), account2.getBalance());
+        System.out.printf("account balance: $%.2f%n", account1.getBalance());
+        System.out.printf("account2 balance: $%.2f%n%n", account2.getBalance());
 
         System.out.print("Enter withdrawal amount for account1: ");
         double amount1 = scanner.nextDouble();
         account1.debit(amount1);
-        System.out.printf("account balance: $%.2f account2 balance: $%.2f\n", account1.getBalance(), account2.getBalance());
+        System.out.printf("account balance: $%.2f%n", account1.getBalance());
+        System.out.printf("account2 balance: $%.2f%n%n", account2.getBalance());
 
         System.out.print("Enter withdrawal amount for account2: ");
         double amount2 = scanner.nextDouble();
         account2.debit(amount2);
-        System.out.printf("account balance: $%.2f account2 balance: $%.2f\n", account1.getBalance(), account2.getBalance());
+        System.out.printf("account balance: $%.2f%n", account1.getBalance());
+        System.out.printf("account2 balance: $%.2f%n%n", account2.getBalance());
     }
 }
