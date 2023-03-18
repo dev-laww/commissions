156f522d127499a7c5dbc81cacc1d72d00156d28
tora-o <97511605+tora-o@users.noreply.github.com>
2023-03-18 19:19:41 +0800

    java: finished account class

--- java/shinji/AccountDebit/src/Account.java
+++ java/shinji/AccountDebit/src/Account.java
@@ -1,3 +1,21 @@
 public class Account {
+    private double balance;
 
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void debit(double amount) {
+        System.out.println(
+                (amount > balance) ?
+                        "Debit amount exceeded account balance." :
+                        "subtracting " + amount + " from account balance"
+        );
+
+        if (amount < balance) balance -= amount;
+    }
 }
