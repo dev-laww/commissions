a92542c6cb8a010540b9b5800c822476aceb3cac
tora-o <97511605+tora-o@users.noreply.github.com>
2023-03-19 13:33:49 +0800

    cpp: added ability to run again

--- c++/lui/BankingSystem/BankingSystem/Main.cpp
+++ c++/lui/BankingSystem/BankingSystem/Main.cpp
@@ -1,9 +1,31 @@
+#include <iostream>
+#include <Windows.h>
 #include "BankingSystem.h"
+
+using namespace std;
 
 int main() {
 	BankingSystem system;
 
 	system.run();
+
+    char again;
+
+    do {
+        cout << "Login again? (y/n) ";
+        cin >> again;
+
+        if (tolower(again) != 'y' && tolower(again) != 'n') {
+            cout << "Invalid choice! Try again." << endl;
+            Sleep(500);
+            continue;
+        }
+
+        if (tolower(again) == 'y') {
+            system.run();
+        }
+
+    } while (tolower(again) != 'n');
 
 	return 0;
 }
