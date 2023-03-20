afdeecea2795640be46ba218aceeadd1f45ee1f8
tora-o <97511605+tora-o@users.noreply.github.com>
2023-03-20 16:08:25 +0800

    java: created book and author class

---
+++ java/gominsi/ApplicationOfMethods/src/module3/exercise3_2methodsAndWithoutParameters/Author.java
@@ -0,0 +1,8 @@
+package module3.exercise3_2methodsAndWithoutParameters;
+
+public class Author {
+    int authorId;
+
+    String lastName;
+    String firstName;
+}
---
+++ java/gominsi/ApplicationOfMethods/src/module3/exercise3_2methodsAndWithoutParameters/Book.java
@@ -0,0 +1,9 @@
+package module3.exercise3_2methodsAndWithoutParameters;
+
+public class Book {
+    int bookId;
+    String title;
+    String ISBN;
+    String category;
+
+}
