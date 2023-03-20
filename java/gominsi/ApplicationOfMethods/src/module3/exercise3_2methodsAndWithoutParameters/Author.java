1a81e1035b5dd434856c3f2725f08b94d6e23d78
tora-o <97511605+tora-o@users.noreply.github.com>
2023-03-20 16:20:03 +0800

    java: created set info method for author

--- java/gominsi/ApplicationOfMethods/src/module3/exercise3_2methodsAndWithoutParameters/Author.java
+++ java/gominsi/ApplicationOfMethods/src/module3/exercise3_2methodsAndWithoutParameters/Author.java
@@ -1,8 +1,22 @@
 package module3.exercise3_2methodsAndWithoutParameters;
 
+import java.util.Scanner;
+
 public class Author {
     int authorId;
-
     String lastName;
     String firstName;
+
+    public void setInfo() {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Enter author ID: ");
+        this.authorId = scanner.nextInt();
+        scanner.nextLine();
+        System.out.println("Enter author's last name: ");
+        this.lastName = scanner.nextLine();
+        System.out.println("Enter author's first name: ");
+        this.firstName = scanner.nextLine();
+
+    }
 }
