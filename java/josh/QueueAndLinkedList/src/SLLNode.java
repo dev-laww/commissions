8165bed2655f17b5583e5954743f7e1c8a66266d
tora-o <97511605+tora-o@users.noreply.github.com>
2023-03-29 14:28:33 +0800

    java: created sll node

---
+++ java/josh/QueueAndLinkedList/src/SLLNode.java
@@ -0,0 +1,25 @@
+public class SLLNode<T> {
+    private T info;
+    private SLLNode<T> next;
+
+    public SLLNode(T info) {
+        this.info = info;
+        this.next = null;
+    }
+
+    public T getInfo() {
+        return info;
+    }
+
+    public void setInfo(T info) {
+        this.info = info;
+    }
+
+    public SLLNode<T> getNext() {
+        return next;
+    }
+
+    public void setNext(SLLNode<T> next) {
+        this.next = next;
+    }
+}
