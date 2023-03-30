19c0df4ec287ff1a650ed4cec3bc0bbabe3b2967
tora-o <97511605+tora-o@users.noreply.github.com>
2023-03-30 20:22:09 +0800

    python: code clean up

--- python/xi.kht/CurrencyConverter/main.py
+++ python/xi.kht/CurrencyConverter/main.py
@@ -12,15 +12,47 @@ class CurrencyConverter:
         self.converted_amount_var = tk.StringVar()
 
         # Create widgets
-        tk.Label(self.master, text="From Currency:").grid(row=0, column=0)
-        tk.OptionMenu(self.master, self.from_currency_var, *self.currencies.keys()).grid(row=0, column=1)
-        tk.Label(self.master, text="To Currency:").grid(row=1, column=0)
-        tk.OptionMenu(self.master, self.to_currency_var, *self.currencies.keys()).grid(row=1, column=1)
-        tk.Label(self.master, text="Amount:").grid(row=2, column=0)
-        tk.Entry(self.master, textvariable=self.amount_var).grid(row=2, column=1)
-        tk.Button(self.master, text="Convert", command=self.convert).grid(row=3, column=1)
-        tk.Label(self.master, text="Converted Amount:").grid(row=4, column=0)
-        tk.Label(self.master, textvariable=self.converted_amount_var).grid(row=4, column=1)
+        tk.Label(
+            self.master,
+            text="From Currency:"
+        ).grid(row=0, column=0, padx=10, pady=10)
+        tk.OptionMenu(
+            self.master,
+            self.from_currency_var,
+            *self.currencies.keys()
+        ).grid(row=0, column=1, padx=10, pady=10)
+
+        tk.Label(
+            self.master,
+            text="To Currency:"
+        ).grid(row=1, column=0, padx=10, pady=10)
+        tk.OptionMenu(
+            self.master,
+            self.to_currency_var,
+            *self.currencies.keys()
+        ).grid(row=1, column=1, padx=10, pady=10)
+
+        tk.Label(
+            self.master,
+            text="Amount:"
+        ).grid(row=2, column=0, padx=10, pady=10)
+        tk.Entry(
+            self.master,
+            textvariable=self.amount_var
+        ).grid(row=2, column=1, padx=10, pady=10)
+        tk.Button(
+            self.master,
+            text="Convert",
+            command=self.convert
+        ).grid(row=3, column=1, padx=10, pady=10)
+        tk.Label(
+            self.master,
+            text="Converted Amount:"
+        ).grid(row=4, column=0, padx=10, pady=10)
+        tk.Label(
+            self.master,
+            textvariable=self.converted_amount_var
+        ).grid(row=4, column=1, padx=10, pady=10)
 
     def convert(self):
         from_currency = self.from_currency_var.get()
