75adb1ba8f3f76e4c1c925e50b7a38a439006868
tora-o <97511605+tora-o@users.noreply.github.com>
2023-03-30 21:46:54 +0800

    python: added styles

--- python/xi.kht/CurrencyConverter/main.py
+++ python/xi.kht/CurrencyConverter/main.py
@@ -1,4 +1,5 @@
 import tkinter as tk
+from tkinter import ttk
 
 import requests
 
@@ -14,59 +15,78 @@ class CurrencyConverter:
         self.amount_var = tk.StringVar()
         self.converted_amount_var = tk.StringVar()
 
+        self._create_styles()
         self._create_widgets()
+
+    def _create_styles(self):
+        # Create the styles for the widgets
+        self.style = ttk.Style()
+
+        # Configure the labels
+        self.style.configure("TLabel", padding=(10, 5))
+
+        # Configure the option menus
+        self.style.configure("TMenubutton", padding=(10, 5))
+
+        # Configure the entry widgets
+        self.style.configure("TEntry", padding=(10, 5))
+
+        # Configure the buttons
+        self.style.configure("TButton", padding=(10, 5))
 
     def _create_widgets(self):
         # Create the input widgets
-        tk.Label(
+        ttk.Label(
             self.master,
             text="From Currency:"
-        ).grid(row=0, column=0, padx=10, pady=10)
-        tk.OptionMenu(
+        ).grid(row=0, column=0)
+        ttk.OptionMenu(
             self.master,
             self.from_currency_var,
+            'USD',
             *['USD', 'EUR']
         ).grid(row=0, column=1, padx=10, pady=10)
 
-        tk.Label(
+        ttk.Label(
             self.master,
             text="To Currency:"
         ).grid(row=1, column=0, padx=10, pady=10)
-        tk.OptionMenu(
+        ttk.OptionMenu(
             self.master,
             self.to_currency_var,
-            *['USD', 'EUR']
+            'EUR',
+            *['USD', 'EUR'],
         ).grid(row=1, column=1, padx=10, pady=10)
 
-        tk.Label(
+        ttk.Label(
             self.master,
             text="Amount:"
         ).grid(row=2, column=0, padx=10, pady=10)
-        tk.Entry(
+        ttk.Entry(
             self.master,
             textvariable=self.amount_var
         ).grid(row=2, column=1, padx=10, pady=10)
 
         # Create the action widgets
-        tk.Button(
+        ttk.Button(
             self.master,
             text="Convert",
             command=self.convert
         ).grid(row=3, column=1, padx=10, pady=10)
-        tk.Label(
+        ttk.Label(
             self.master,
             text="Converted Amount:"
         ).grid(row=4, column=0, padx=10, pady=10)
-        tk.Label(
+        ttk.Label(
             self.master,
             textvariable=self.converted_amount_var
         ).grid(row=4, column=1, padx=10, pady=10)
 
         # Create the "Swap" button
-        tk.Button(
+        ttk.Button(
             self.master,
             text="Swap",
-            command=self.swap_currencies
+            command=self.swap_currencies,
         ).grid(row=0, column=2, rowspan=2, padx=10, pady=10)
 
     def convert(self):
