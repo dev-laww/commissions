287ea26f2cb339393d6e829dc97f4ab5574c664e
tora-o <97511605+tora-o@users.noreply.github.com>
2023-04-24 16:51:26 +0800

    java: DatabaseHandler update

--- java/maika/ATMWithDB/src/finalproject/db/DatabaseHandler.java
+++ java/maika/ATMWithDB/src/finalproject/db/DatabaseHandler.java
@@ -3,36 +3,79 @@ package finalproject.db;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.ArrayList;
 
 public class DatabaseHandler {
+    public static ArrayList<User> users = new ArrayList<>();
+    public static ArrayList<Transaction> transactions = new ArrayList<>();
+    static {
+        try {
+            users.addAll(UserHandler.getAll());
+//            transactions.addAll(TransactionHandler.getAll());
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     public static void deleteUser(String id) throws SQLException {
-        UserHandler model = new UserHandler(UserHandler.getUser(id));
+        User user = UserHandler.getUser(id);
+        UserHandler model = new UserHandler(user);
         model.delete();
+
+        for (User it : users) {
+            if (id.equals(it.id)) {
+                users.remove(it);
+                break;
+            }
+        }
     }
 
     public static void deleteTransaction(String id) throws SQLException {
         TransactionHandler model = new TransactionHandler(TransactionHandler.getTransaction(id));
         model.delete();
+
+        for (Transaction it : transactions) {
+            if (id.equals(it.id)) {
+                transactions.remove(it);
+                break;
+            }
+        }
     }
 
     public static void addUser(User user) throws SQLException {
         UserHandler model = new UserHandler(user);
         model.save();
+        users.add(user);
     }
 
     public static void addTransaction(Transaction transaction) throws SQLException {
         TransactionHandler model = new TransactionHandler(transaction);
         model.save();
+        transactions.add(transaction);
     }
 
     public static void updateUser(User user) throws SQLException {
         UserHandler model = new UserHandler(user);
         model.save();
+
+        for (int i = 0; i < users.size(); i++) {
+            if (user.id.equals(users.get(i).id)) {
+                users.set(i, user);
+                break;
+            }
+        }
     }
 
     public static void updateTransaction(Transaction transaction) throws SQLException {
         TransactionHandler model = new TransactionHandler(transaction);
         model.save();
+
+        for (int i = 0; i < transactions.size(); i++) {
+            if (transaction.id.equals(transactions.get(i).id)) {
+                transactions.set(i, transaction);
+                break;
+            }
+        }
     }
 
     public static Connection getConnection() {
@@ -49,5 +92,25 @@ public class DatabaseHandler {
         }
 
         return conn;
+    }
+
+    public static User getUser(String id) {
+        for (User user : users) {
+            if (id.equals(user.id)) {
+                return user;
+            }
+        }
+
+        return null;
+    }
+
+    public static Transaction getTransaction(String id) {
+        for (Transaction transaction : transactions) {
+            if (id.equals(transaction.id)) {
+                return transaction;
+            }
+        }
+
+        return null;
     }
 }
