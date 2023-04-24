1faf5da178fa64dc78d403f980dfae4b1f24f377
tora-o <97511605+tora-o@users.noreply.github.com>
2023-04-24 09:07:55 +0800

    java: updates on transaction class

--- java/maika/ATMWithDB/src/finalproject/db/Transaction.java
+++ java/maika/ATMWithDB/src/finalproject/db/Transaction.java
@@ -4,13 +4,26 @@ import java.util.Date;
 
 public class Transaction {
     public String id;
-    private String userID;
-    private double amount;
-    private String type;
+    private final String userID;
+    public final double amount;
+    public final String type;
 
     Transaction(String userID, double amount, String type) {
         this.userID = userID;
         this.amount = amount;
         this.type = type;
     }
+
+    Transaction(String id, String userID, double amount, String type) {
+        this.id = id;
+        this.userID = userID;
+        this.amount = amount;
+        this.type = type;
+    }
+
+    public String userID() {
+        return this.userID;
+    }
+
+
 }
