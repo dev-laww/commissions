9e3e5878c67eb2c6ac14803cf3f078d121314f84
tora-o <97511605+tora-o@users.noreply.github.com>
2023-05-07 17:39:42 +0800

    java: updated db

--- java/maika/ATMWithDB/db.sql
+++ java/maika/ATMWithDB/db.sql
@@ -18,6 +18,51 @@ USE `atm`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `dropped_transactions`
+--
+
+DROP TABLE IF EXISTS `dropped_transactions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dropped_transactions` (
+  `id` bigint NOT NULL,
+  `user_id` bigint NOT NULL,
+  `transfer_user_id` bigint DEFAULT NULL,
+  `type` varchar(20) NOT NULL,
+  `amount` double NOT NULL,
+  `datetime` timestamp NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `user_id` (`user_id`),
+  CONSTRAINT `dropped_user_id` FOREIGN KEY (`user_id`) REFERENCES `dropped_users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `dropped_users`
+--
+
+DROP TABLE IF EXISTS `dropped_users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `dropped_users` (
+  `id` bigint NOT NULL,
+  `status` varchar(10) NOT NULL,
+  `first_name` varchar(70) NOT NULL,
+  `middle_name` varchar(25) NOT NULL,
+  `last_name` varchar(25) NOT NULL,
+  `barangay` text NOT NULL,
+  `municipality` text NOT NULL,
+  `province` text NOT NULL,
+  `contact` varchar(11) NOT NULL,
+  `email` text NOT NULL,
+  `pin` varchar(4) NOT NULL,
+  `balance` float NOT NULL,
+  `atm_id` varchar(8) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Table structure for table `transactions`
 --
 
@@ -27,13 +72,14 @@ DROP TABLE IF EXISTS `transactions`;
 CREATE TABLE `transactions` (
   `id` bigint NOT NULL AUTO_INCREMENT,
   `user_id` bigint DEFAULT NULL,
+  `transfer_user_id` bigint DEFAULT NULL,
   `type` varchar(20) NOT NULL,
   `amount` double NOT NULL,
   `datetime` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   KEY `user_id` (`user_id`),
   CONSTRAINT `user_id` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -44,14 +90,19 @@ DROP TABLE IF EXISTS `users`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `name` varchar(70) DEFAULT NULL,
-  `address` text,
-  `contact` varchar(11) DEFAULT NULL,
-  `email` text,
-  `pin` varchar(4) DEFAULT NULL,
-  `status` varchar(10) DEFAULT NULL,
   `id` bigint NOT NULL AUTO_INCREMENT,
-  `balance` float DEFAULT NULL,
+  `status` varchar(10) NOT NULL,
+  `first_name` varchar(70) NOT NULL,
+  `middle_name` varchar(25) NOT NULL,
+  `last_name` varchar(25) NOT NULL,
+  `barangay` text NOT NULL,
+  `municipality` text NOT NULL,
+  `province` text NOT NULL,
+  `contact` varchar(11) NOT NULL,
+  `email` text NOT NULL,
+  `pin` varchar(4) NOT NULL,
+  `balance` float NOT NULL,
+  `atm_id` varchar(8) NOT NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB AUTO_INCREMENT=10000000 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
@@ -65,4 +116,4 @@ CREATE TABLE `users` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2023-04-29 18:27:40
+-- Dump completed on 2023-05-07 17:35:18
